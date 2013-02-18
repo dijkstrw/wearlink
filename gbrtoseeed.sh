8887d1b663a2459001a326acf72a6ec44fa93bbc
Willem Dijkstra <wpd@xs4all.nl>
2013-02-18 12:50:34 +0100

    Preparing for fabrication

---
+++ gbrtoseeed.sh
@@ -0,0 +1,20 @@
+for i in *.gbr; do
+    name=${i/.*}
+    t=${i#${name}.}
+    t=${t%.gbr}
+    echo $i -- $t
+
+    case $t in
+        top) t=GTL ;;
+        topmask) t=GTS ;;
+        topsilk) t=GTO ;;
+        bottom) t=GBL ;;
+        bottommask) t=GBS ;;
+        bottomsilk) t=GBO ;;
+        *) t="" ;;
+    esac
+
+    if [ -n "$t" ]; then
+        mv $i $name.$t
+    fi
+done

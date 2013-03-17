d27f9c0e78672bd3f1e265a1419df91575839c57
Willem Dijkstra <wpd@xs4all.nl>
2013-03-17 12:27:19 +0100

    Added quick AVR ISP IDC6 -> WL ISP IDC6 pcb as a soldering template

---
+++ connectors.cmd
@@ -0,0 +1,49 @@
+# Pin name action command file
+
+# Start of element ISP_WL
+ChangePinName(ISP_WL, 6, 6)
+ChangePinName(ISP_WL, 5, 5)
+ChangePinName(ISP_WL, 1, 1)
+ChangePinName(ISP_WL, 4, 4)
+ChangePinName(ISP_WL, 3, 3)
+ChangePinName(ISP_WL, 2, 2)
+
+# Start of element STD_ISP6
+ChangePinName(STD_ISP6, 6, 6)
+ChangePinName(STD_ISP6, 5, 5)
+ChangePinName(STD_ISP6, 1, 1)
+ChangePinName(STD_ISP6, 4, 4)
+ChangePinName(STD_ISP6, 3, 3)
+ChangePinName(STD_ISP6, 2, 2)
+
+# Start of element RESET
+ChangePinName(RESET, 1, none)
+ChangePinName(RESET, 1, none)
+ChangePinName(RESET, 1, none)
+
+# Start of element SCK
+ChangePinName(SCK, 1, none)
+ChangePinName(SCK, 1, none)
+ChangePinName(SCK, 1, none)
+
+# Start of element MISO
+ChangePinName(MISO, 1, none)
+ChangePinName(MISO, 1, none)
+ChangePinName(MISO, 1, none)
+
+# Start of element MOSI
+ChangePinName(MOSI, 1, none)
+ChangePinName(MOSI, 1, none)
+ChangePinName(MOSI, 1, none)
+
+# Start of element STD_ISP10
+ChangePinName(STD_ISP10, 6, 6)
+ChangePinName(STD_ISP10, 7, 7)
+ChangePinName(STD_ISP10, 8, 8)
+ChangePinName(STD_ISP10, 9, 9)
+ChangePinName(STD_ISP10, 10, 10)
+ChangePinName(STD_ISP10, 5, 5)
+ChangePinName(STD_ISP10, 1, 1)
+ChangePinName(STD_ISP10, 4, 4)
+ChangePinName(STD_ISP10, 3, 3)
+ChangePinName(STD_ISP10, 2, 2)

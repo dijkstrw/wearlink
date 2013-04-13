175d3984fefc476fe2713197de8a589af4eb7fb3
Willem Dijkstra <wpd@xs4all.nl>
2013-04-13 15:12:55 +0200

    Restructured project directory in preparation for documentation writing.

---
+++ layout/wearlink.cmd
@@ -0,0 +1,62 @@
+# Pin name action command file
+
+# Start of element FBTRNIC
+ChangePinName(FBTRNIC, 3, 3)
+ChangePinName(FBTRNIC, 1, 1)
+ChangePinName(FBTRNIC, 2, 2)
+
+# Start of element ISP
+ChangePinName(ISP, 6, 6)
+ChangePinName(ISP, 5, 5)
+ChangePinName(ISP, 1, 1)
+ChangePinName(ISP, 4, 4)
+ChangePinName(ISP, 3, 3)
+ChangePinName(ISP, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element Z2
+ChangePinName(Z2, 2, 2)
+ChangePinName(Z2, 1, 1)
+
+# Start of element Z1
+ChangePinName(Z1, 2, 2)
+ChangePinName(Z1, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 5, "(MOSI) PB0")
+ChangePinName(U1, 6, "(MISO) PB1")
+ChangePinName(U1, 7, "(SCK/INT0) PB2")
+ChangePinName(U1, 2, PB3)
+ChangePinName(U1, 3, "(PCINT4/ADC2) PB4")
+ChangePinName(U1, 1, "(RESET/DW) PB5")
+ChangePinName(U1, 8, VCC)
+ChangePinName(U1, 4, GND)
+
+# Start of element USB
+ChangePinName(USB, 4, GND)
+ChangePinName(USB, 1, VCC)
+ChangePinName(USB, 2, D-)
+ChangePinName(USB, 3, D+)
